fed9ca959faff2d26d1c4fd390f880f0a66c1672
Simone Bernardini <simone.bernardini@thefloow.com>
2021-11-27 16:55:22 +0000

    Added binding to specify how to inject contexts when required

---
+++ app/src/main/java/com/example/freeweather/hilt/modules/AppModule.kt
@@ -0,0 +1,19 @@
+package com.example.freeweather.hilt.modules
+
+import android.app.Application
+import android.content.Context
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class AppModule {
+
+    @Binds
+    @Singleton
+    abstract fun applicationContext(application: Application): Context
+
+}
