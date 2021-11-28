eadc0ab80d8e335d73501fd0895ee37935ae6f3f
Simone Bernardini <simone.bernardini@thefloow.com>
2021-11-28 16:05:18 +0000

    App splashscreen using new A12 api

--- app/src/main/java/com/example/freeweather/presentation/NavigationActivity.kt
+++ app/src/main/java/com/example/freeweather/presentation/NavigationActivity.kt
@@ -2,10 +2,17 @@ package com.example.freeweather.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.freeweather.R
+import com.example.freeweather.databinding.ActivityNavigationBinding
 
 class NavigationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNavigationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_navigation)
+        binding = ActivityNavigationBinding.inflate(layoutInflater)
+        installSplashScreen()
+        setContentView(binding.root)
     }
 }
