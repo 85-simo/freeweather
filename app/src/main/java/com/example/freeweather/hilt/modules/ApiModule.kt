1fae3288a9893fe71f97b3863c30ce69191b4093
Simone Bernardini <simone.bernardini@thefloow.com>
2021-11-28 02:33:23 +0000

    Updated Hilt config

--- app/src/main/java/com/example/freeweather/hilt/modules/ApiModule.kt
+++ app/src/main/java/com/example/freeweather/hilt/modules/ApiModule.kt
@@ -1,9 +1,12 @@
 package com.example.freeweather.hilt.modules
 
 import android.content.Context
+import com.example.freeweather.data.api.RestClient
+import com.example.freeweather.data.api.RestClientImpl
 import com.example.freeweather.data.api.RestGeocoding
 import com.example.freeweather.data.api.RestWeather
 import com.google.gson.Gson
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -29,52 +32,58 @@ private const val API_KEY_VALUE = "0db5caf45f1986305c1379cb056d6d34"
 
 @InstallIn(SingletonComponent::class)
 @Module
-object ApiModule {
+abstract class ApiModule {
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideHttpLoggingInterceptor() = Interceptor { chain ->
-        val originalReq = chain.request()
-        val originalUrl = originalReq.url
-        val newUrl = originalUrl.newBuilder()
-            .addQueryParameter(API_KEY_PARAM_NAME, API_KEY_VALUE)
+    internal abstract fun bindRestClient(client: RestClientImpl): RestClient
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideHttpLoggingInterceptor() = Interceptor { chain ->
+            val originalReq = chain.request()
+            val originalUrl = originalReq.url
+            val newUrl = originalUrl.newBuilder()
+                .addQueryParameter(API_KEY_PARAM_NAME, API_KEY_VALUE)
+                .build()
+            val newRequest = originalReq.newBuilder()
+                .url(newUrl)
+                .build()
+            chain.proceed(newRequest)
+        }
+
+        @Provides
+        @Singleton
+        fun provideApiCache(context: Context): Cache {
+            val cacheFile = File("${context.cacheDir.absolutePath}${File.pathSeparator}$API_CACHE_DIR")
+            return Cache(cacheFile, API_CACHE_SIZE)
+        }
+
+        @Provides
+        @Singleton
+        fun provideHttpClient(interceptor: Interceptor, apiCache: Cache) = OkHttpClient.Builder()
+            .connectTimeout(REST_CLIENT_CONNECTION_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .readTimeout(REST_CLIENT_READ_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .writeTimeout(REST_CLIENT_WRITE_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .cache(apiCache)
+            .addInterceptor(interceptor)
             .build()
-        val newRequest = originalReq.newBuilder()
-            .url(newUrl)
+
+        @Provides
+        @Singleton
+        fun provideRetrofit(httpClient: OkHttpClient, gson: Gson) = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .baseUrl(BASE_URL)
+            .client(httpClient)
             .build()
-        chain.proceed(newRequest)
+
+        @Provides
+        @Singleton
+        fun provideRestWeatherInterface(retrofit: Retrofit) = retrofit.create(RestWeather::class.java)
+
+        @Provides
+        @Singleton
+        fun provideRestGeocodingInterface(retrofit: Retrofit) = retrofit.create(RestGeocoding::class.java)
     }
-
-    @Provides
-    @Singleton
-    fun provideApiCache(context: Context): Cache {
-        val cacheFile = File("${context.cacheDir.absolutePath}${File.pathSeparator}$API_CACHE_DIR")
-        return Cache(cacheFile, API_CACHE_SIZE)
-    }
-
-    @Provides
-    @Singleton
-    fun provideHttpClient(interceptor: Interceptor, apiCache: Cache) = OkHttpClient.Builder()
-        .connectTimeout(REST_CLIENT_CONNECTION_TIMEOUT_SECONDS, TimeUnit.SECONDS)
-        .readTimeout(REST_CLIENT_READ_TIMEOUT_SECONDS, TimeUnit.SECONDS)
-        .writeTimeout(REST_CLIENT_WRITE_TIMEOUT_SECONDS, TimeUnit.SECONDS)
-        .cache(apiCache)
-        .addInterceptor(interceptor)
-        .build()
-
-    @Provides
-    @Singleton
-    fun provideRetrofit(httpClient: OkHttpClient, gson: Gson) = Retrofit.Builder()
-        .addConverterFactory(GsonConverterFactory.create(gson))
-        .baseUrl(BASE_URL)
-        .client(httpClient)
-        .build()
-
-    @Provides
-    @Singleton
-    fun provideRestWeatherInterface(retrofit: Retrofit) = retrofit.create(RestWeather::class.java)
-
-    @Provides
-    @Singleton
-    fun provideRestGeocodingInterface(retrofit: Retrofit) = retrofit.create(RestGeocoding::class.java)
 }
--- app/src/main/java/com/example/freeweather/hilt/modules/PersistenceModule.kt
+++ app/src/main/java/com/example/freeweather/hilt/modules/PersistenceModule.kt
@@ -3,6 +3,9 @@ package com.example.freeweather.hilt.modules
 import android.content.Context
 import androidx.room.Room
 import com.example.freeweather.data.db.AppDatabase
+import com.example.freeweather.data.repository.Repository
+import com.example.freeweather.data.repository.RepositoryImpl
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,13 +16,19 @@ private const val DB_NAME = "database.db"
 
 @InstallIn(SingletonComponent::class)
 @Module
-object PersistenceModule {
+abstract class PersistenceModule {
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideDatabase(applicationContext: Context) = Room.databaseBuilder(
-        applicationContext,
-        AppDatabase::class.java,
-        DB_NAME
-    )
+    internal abstract fun bindRepository(repositoryImpl: RepositoryImpl): Repository
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideDatabase(applicationContext: Context) = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            DB_NAME
+        )
+    }
 }
