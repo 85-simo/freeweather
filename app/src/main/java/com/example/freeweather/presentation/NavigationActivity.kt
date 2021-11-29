426f562132f63970c5ecb6467e455c669a98f03f
Simone Bernardini <simone.bernardini@thefloow.com>
2021-11-29 02:19:23 +0000

    Set toolbar in main activity

--- app/src/main/java/com/example/freeweather/presentation/NavigationActivity.kt
+++ app/src/main/java/com/example/freeweather/presentation/NavigationActivity.kt
@@ -1,11 +1,12 @@
 package com.example.freeweather.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.example.freeweather.R
 import com.example.freeweather.databinding.ActivityNavigationBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NavigationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNavigationBinding
 
